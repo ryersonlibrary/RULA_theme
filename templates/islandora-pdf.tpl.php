8e597adbbedc8340517cb74f622808365ff34bc4
MJ Suhonos <mj@suhonos.ca>
2015-08-21 16:14:18 -0400

    Template alignment updates

--- templates/islandora-pdf.tpl.php
+++ templates/islandora-pdf.tpl.php
@@ -11,11 +11,26 @@
 <div class="islandora-pdf-object islandora" vocab="http://schema.org/" prefix="dcterms: http://purl.org/dc/terms/" typeof="Article">
   <div class="islandora-pdf-content-wrapper clearfix">
     <?php if (isset($islandora_content)): ?>
-      <div class="islandora-pdf-content col-md-7">
+      <div class="islandora-pdf-content-new col-md-7">
         <?php print $islandora_content; ?>
       <?php if (isset($islandora_download_link)): ?>
         <?php print $islandora_download_link; ?>
       <?php endif; ?>
+      <?php print $description; ?>
+      </div>
+
+      <div class="islandora-pdf-metadata col-md-5 pull-right">
+      <?php if($parent_collections): ?>
+      	<div>
+      	  <h2><?php print t('In collections'); ?></h2>
+      		<ul>
+      		  <?php foreach ($parent_collections as $collection): ?>
+      			<li><?php print l($collection->label, "islandora/object/{$collection->id}"); ?></li>
+      	  	  <?php endforeach; ?>
+      		</ul>
+		</div>
+      <?php endif; ?>
+      <?php print $metadata; ?>
       <!-- usage stats -->
       <?php if (module_exists('islandora_usage_stats')): ?>
       	<div class="islandora-usage-stats">
@@ -27,21 +42,6 @@
 	  	</div>
       <?php endif; ?>
       <!-- end usage stats -->
-      <?php print $description; ?>
-      <?php if($parent_collections): ?>
-      	<div>
-      	  <h2><?php print t('In collections'); ?></h2>
-      		<ul>
-      		  <?php foreach ($parent_collections as $collection): ?>
-      			<li><?php print l($collection->label, "islandora/object/{$collection->id}"); ?></li>
-      	  	  <?php endforeach; ?>
-      		</ul>
-		</div>
-      <?php endif; ?>
-      </div>
-
-      <div class="islandora-pdf-metadata col-md-5 pull-right">
-      <?php print $metadata; ?>
       </div>
     <?php endif; ?>
   </div>
